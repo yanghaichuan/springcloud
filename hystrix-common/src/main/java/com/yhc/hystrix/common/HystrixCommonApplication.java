2a63bea4fb6ef54cbc4a428be22df35273783387
yanghaichuan <51939690+yanghaichuan@users.noreply.github.com>
2019-10-23 15:48:12 +0800

    Hystrix

--- hystrix-common/src/main/java/com/yhc/hystrix/common/HystrixCommonApplication.java
+++ hystrix-common/src/main/java/com/yhc/hystrix/common/HystrixCommonApplication.java
@@ -2,12 +2,12 @@ package com.yhc.hystrix.common;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
+@EnableHystrix
+@EnableEurekaClient
 @SpringBootApplication
-@EnableDiscoveryClient
-@EnableCircuitBreaker
 public class HystrixCommonApplication {
 
     public static void main(String[] args) {
---
+++ hystrix-common/src/main/java/com/yhc/hystrix/common/ProductController.java
@@ -0,0 +1,23 @@
+package com.yhc.hystrix.common;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+    @HystrixCommand(fallbackMethod = "getFallback")
+    @RequestMapping(value = "/product/get/{id}")
+    public String getProduct(@PathVariable("id") Long id) {
+        if (id.intValue() == 1) {
+            throw new RuntimeException("id="+id+"无效");
+        }
+        return "操作成功";
+    }
+
+    public String getFallback(@PathVariable("id") Long id) {
+        return "无效ID=" + id;
+    }
+}
