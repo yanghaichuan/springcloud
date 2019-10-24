e6b75327e77ab439eac87ae956001327eacabfc4
yanghaichuan <51939690+yanghaichuan@users.noreply.github.com>
2019-10-24 11:23:53 +0800

    位置移动

--- hystrix-common/src/main/java/com/yhc/hystrix/common/controller/ProductController.java
+++ hystrix-common/src/main/java/com/yhc/hystrix/common/controller/ProductController.java
@@ -1,4 +1,4 @@
-package com.yhc.hystrix.common;
+package com.yhc.hystrix.common.controller;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.web.bind.annotation.PathVariable;
