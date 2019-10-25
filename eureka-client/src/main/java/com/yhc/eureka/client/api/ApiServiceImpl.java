bc29423fc5bdcc567ec9bf4b516b32c54f48964e
yanghaichuan <51939690+yanghaichuan@users.noreply.github.com>
2019-10-25 15:32:59 +0800

    test1

--- eureka-client/src/main/java/com/yhc/eureka/client/api/ApiServiceImpl.java
+++ eureka-client/src/main/java/com/yhc/eureka/client/api/ApiServiceImpl.java
@@ -12,11 +12,6 @@ public class ApiServiceImpl implements ApiService {
 
     @Override
     public String test() {
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        return "熔断来了";
+        return "test";
     }
 }
--- hystrix-common/src/main/java/com/yhc/hystrix/common/service/HystrixServiceFallback.java
+++ hystrix-common/src/main/java/com/yhc/hystrix/common/service/HystrixServiceFallback.java
@@ -11,7 +11,6 @@ public class HystrixServiceFallback implements HystrixService {
 
     @Override
     public String test() {
-
         return apiService.test();
     }
 }
